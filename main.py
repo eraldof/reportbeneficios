836d2464fab3ff0b8e908dcc220956c0f0970e74
Eraldo Rocha <58483932+eraldof@users.noreply.github.com>
2025-05-08 08:27:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -711,11 +711,11 @@ def process_report2(beneficios_file, bi_path, ednaldo=False, progress_callback=N
         update_progress(100, "Relatório finalizado")
         result_bi = pd.read_excel(bi_path, 
                                   dtype= {'COD CENTRO CUSTO': str, 
-                                        'SINTETICO CC': str,
+                                        'SINTETICO': str,
                                         'CONTA': str, 
                                         'VALOR': float}, 
-                                usecols= ['COD CENTRO CUSTO', 'SINTETICO CC', 'CONTA', 'VALOR'])
-        result_bi = result_bi.rename(columns={'COD CENTRO CUSTO': 'CC', 'SINTETICO CC': 'FILIAL', 'CONTA': 'BENEFICIO'})
+                                usecols= ['COD CENTRO CUSTO', 'SINTETICO', 'CONTA', 'VALOR'])
+        result_bi = result_bi.rename(columns={'COD CENTRO CUSTO': 'CC', 'SINTETICO': 'FILIAL', 'CONTA': 'BENEFICIO'})
         excluded_benefits = ['SUBSIDIO EDUCACAO', 'CURSOS E TREINAMENTOS', 'VALE TRANSPORTE']
         result_bi = result_bi[~result_bi['BENEFICIO'].isin(excluded_benefits)]
         filial_mapping = {
